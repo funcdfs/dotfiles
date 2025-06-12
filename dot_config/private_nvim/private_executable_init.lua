233af4414271633acb560b7d623ab296b596ad54
funcdfs <funcdfs@gmail.com>
2025-06-12 12:07:13 +0800

    Add .config/nvim/.DS_Store Add .config/nvim/.git/.DS_Store Add .config/nvim/.git/HEAD Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/index Add .config/nvim/.git/packed-refs Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/main Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-ecf2a12770f3e1f6ed4a57cca2469cf248d4e1e2.idx Add .config/nvim/.git/objects/pack/pack-ecf2a12770f3e1f6ed4a57cca2469cf248d4e1e2.pack Add .config/nvim/.git/refs/heads/main Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.gitignore Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/readme.md Add .config/nvim/lua/config/indent-blankline.lua Add .config/nvim/lua/config/nvim-autopairs.lua Add .config/nvim/lua/colors.lua Add .config/nvim/lua/keymaps.lua Add .config/nvim/lua/options.lua Add .config/nvim/lua/plugins.lua

---
+++ dot_config/private_nvim/private_executable_init.lua
@@ -0,0 +1,47 @@
+-- load options
+require("options")
+
+-- load keymappings
+require("keymaps")
+
+-- load plugins
+require("plugins")
+
+-- Set colorscheme
+require("colors")
+
+-- Set LSP
+-- require("lsp")
+
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide 
+    vim.o.guifont = "Source Code Pro:h14" -- text below applies for VimScript
+    vim.g.neovide_scroll_animation_length = 0.2
+    vim.g.neovide_cursor_trail_size = 0.3
+    vim.g.neovide_cursor_animation_length = 0.15
+    vim.g.neovide_window_blurred = true
+    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_remember_window_size = true
+    vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    vim.g.neovide_cursor_vfx_particle_density = 1.2
+    vim.g.neovide_opacity = 0.7
+    vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+    vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+    vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+    vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+    vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+
+vim.o.cursorline = true -- Ensure the cursorline is enabled
+-- vim.cmd([[
+--   highlight CursorLine guibg=#000000 guifg=FFFFFF
+-- ]])
